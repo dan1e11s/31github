9e620787787f790f7bc8352c6af4578497ba40f3
Daniel <danielmuratov2003@gmail.com>
2023-11-30 22:09:33 +0600

    quadrants task add

--- codewars/index.js
+++ codewars/index.js
@@ -24,3 +24,17 @@
 //     (numbers[0].toString().slice(0, 1) + numbers[1].toString().slice(1)) * 1;
 //   return Math.abs(n1 - n2);
 // }
+
+//! Quadrants (8kyu)
+
+// function quadrant(x, y) {
+//   if (x > 0 && y > 0) {
+//     return 1;
+//   } else if (x < 0 && y > 0) {
+//     return 2;
+//   } else if (x < 0 && y < 0) {
+//     return 3;
+//   } else if (x > 0 && y < 0) {
+//     return 4;
+//   }
+// }
