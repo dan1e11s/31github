7102b86a53687d79f9569218ccb3e8ad7a60dc8a
Daniel <danielmuratov2003@gmail.com>
2023-12-03 23:21:12 +0600

    add store

--- oxxxymiron/src/index.jsx
+++ oxxxymiron/src/index.jsx
@@ -1,7 +1,17 @@
 import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './components/App/App';
 
+import { store } from './store';
+
 const root = createRoot(document.getElementById('root'));
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
