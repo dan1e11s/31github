b812cdd4d4b302e97483bc8d963bf95fd98f54cb
Daniel <danielmuratov2003@gmail.com>
2023-12-04 06:23:19 +0600

    added styles, images, fonts

--- oxxxymiron/src/components/App/App.jsx
+++ oxxxymiron/src/components/App/App.jsx
@@ -2,6 +2,8 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import AppRoutes from './AppRoutes';
 
+import '../../styles/index.scss';
+
 const App = () => {
   return (
     <div className="app">
