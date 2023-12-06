6e63dfd562963d9988d1691c81efb9c9b78b695f
Daniel <danielmuratov2003@gmail.com>
2023-12-06 20:17:34 +0600

    start tour logic

--- oxxxymiron/src/components/Home/Home.jsx
+++ oxxxymiron/src/components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import MainBanner from './MainBanner';
+import TourItems from '../Tour/TourItems';
 
 const Home = () => {
   return (
     <main className="main">
       <MainBanner />
+      <TourItems />
     </main>
   );
 };
---
+++ oxxxymiron/src/components/Tour/TourItems.jsx
@@ -0,0 +1,14 @@
+import Section from '../Section/Section';
+import SectionTitle from '../SectionTitle/SectionTitle';
+
+const TourItems = () => {
+  return (
+    <Section className="tour">
+      <div className="container">
+        <SectionTitle text="Концерты" />
+      </div>
+    </Section>
+  );
+};
+
+export default TourItems;
